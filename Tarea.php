92cb1d7f460aa9da1a9f7b8dc724296807ce7244
gallegos693 <gallegos693@hotmail.com>
2018-04-16 00:42:03 -0500

    Tarea

---
+++ Tarea.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Ejemplo de PHP</title>
+</head>
+<body>
+<H1>Tarea</H1>
+<FORM ACTION="parametros.php" METHOD="GET"
+<?php echo "<p>Nombre:Edgar Gallegos Franco</p>"; ?>
+<?php echo "<p>Numero de cuenta: 305165891</p>"; ?>
+</FORM>
+</body>
+</html>
